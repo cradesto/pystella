474d0cd63186e2adae8161eba7ae042d7b13d611
bakl <baklanovp@gmail.com>
2016-01-19 14:59:57 +0900

    - CHG: move velocities function to new file

--- ubv.py
+++ ubv.py
@@ -16,6 +16,7 @@ import matplotlib.pyplot as plt
 from pystella.model.stella import Stella
 from pystella.rf import band
 from pystella.rf import extinction
+import velocity as vel
 
 __author__ = 'bakl'
 
@@ -81,45 +82,12 @@ def plot_all(models_vels, models_dic, bands, callback=None, xlim=None, ylim=None
 
     # plot velocities
     if is_vel:
-        plot_vels_models(axVel, models_vels, xlim=axUbv.get_xlim())
-        plot_vels_sn87a(axVel, z=1.49)
+        vel.plot_vels_models(axVel, models_vels, xlim=axUbv.get_xlim())
+        vel.plot_vels_sn87a(axVel, z=1.49)
         axVel.legend(prop={'size': 8}, loc=4)
 
     plt.grid()
     plt.show()
-
-
-def plot_vels_models(ax, models_dic, xlim=None, ylim=None):
-    is_x_lim = xlim is None
-    is_y_lim = ylim is None
-
-    t_points = [0.2, 1, 2, 3, 4, 5, 10, 20, 40, 80, 150]
-
-    lw = 1.
-    mi = 0
-    x_max = []
-    y_mid = []
-    for mname, mdic in models_dic.iteritems():
-        mi += 1
-        x = mdic['time']
-        y = mdic['vel'] / 1e8
-        ax.plot(x, y, label='Vel  %s' % mname, color='blue', ls="-", linewidth=lw)
-        if is_x_lim:
-            x_max.append(np.max(x))
-        if is_y_lim:
-            y_mid.append(np.max(y))
-
-    if is_x_lim:
-        xlim = [-10, np.max(x_max) + 10.]
-    ax.set_xlim(xlim)
-
-    if is_y_lim:
-        ylim = [1e-1, np.max(y_mid) + 5]
-        # ylim = [np.min(y_mid) + 7., np.min(y_mid) - 2.]
-    ax.set_ylim(ylim)
-
-    ax.set_ylabel('Velocity')
-    ax.set_xlabel('Time [days]')
 
 
 def plot_ubv_models(ax, models_dic, bands, band_shift, xlim=None, ylim=None, is_time_points=True):
@@ -362,29 +330,6 @@ def plot_tolstov(ax, band_shift):
         bcolor = colors[fs[i]]
         ax.plot(x, y, label='%s Tolstov' % lbl(fs[i], band_shift),
                 color=bcolor, ls="-.", linewidth=lw)
-
-
-def plot_vels_sn87a(ax, z=0):
-    print "Plot the velocities of Sn 87A "
-    d = os.path.expanduser('~/Sn/Release/svn_kepler/stella/branches/lucy/run/res/sncurve/sn1987a')
-
-    jd_shift = 2446850  # moment of explosion SN 1987A, Hamuy 1988, doi:10.1086/114613
-
-    # Blanco's data from plot
-    fs = {'Halpha': os.path.join(d, 'Halpha_blanco.csv'), 'Hbeta': os.path.join(d, 'Hbeta_blanco.csv'),
-          'Hgamma': os.path.join(d, 'Hgamma_blanco.csv'), 'NaID': os.path.join(d, 'NaID_blanco.csv'),
-          'FeII5018': os.path.join(d, 'FeII5018_blanco.csv'), 'FeII5169': os.path.join(d, 'FeII5169_blanco.csv')
-          }
-    elcolors = {'Halpha': "black", 'Hbeta': "cyan", 'Hgamma': "orange", 'NaID': "red", 'FeII5018': "orange",
-                'FeII5169': "magenta"}
-    elmarkers = {'Halpha': u's', 'Hbeta': u'x', 'Hgamma': u'd', 'NaID': u'+', 'FeII5018': u'D', 'FeII5169': u'o'}
-
-    for el, fname in fs.items():
-        data = np.loadtxt(fname, comments='#')
-        x = data[:, 0] - jd_shift
-        x *= 1. + z  # redshift
-        y = data[:, 1]
-        ax.plot(x, y, label='%s, SN 87A' % el, ls=".", color=elcolors[el], markersize=6, marker=elmarkers[el])
 
 
 def plot_snrefsdal(ax, band_shift, arg=None):
