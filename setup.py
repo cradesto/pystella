15ba0ebd6ed93937eed922673312e204b24d6779
baklanovp <baklanovp@gmail.com>
2017-02-10 19:52:17 +0300

    - ADD: setup sdist with data (bands, etc)

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
-
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
+#
+# For packing data see https://danielsokolowski.blogspot.ru/2012/08/setuptools-includepackagedata-option.html
+# $ python3 setup.py sdist
+#
 
 
 def read(fname):
@@ -21,7 +20,8 @@ setup(
     license="MIT",
     keywords="supernova, light curves",
     url="https://github.com/baklanovp/pystella",
-    packages=['pystella', 'pystella.fit', 'pystella.model', 'pystella.rf', 'pystella.util'],
+    packages=find_packages(),
+    # packages=['pystella', 'pystella.fit', 'pystella.model', 'pystella.rf', 'pystella.util'],
     # data_files=[('', ['data/*'])],
     # package_data={'': ['data/']},
     include_package_data=True,
