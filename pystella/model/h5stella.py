c204894bc85f002cbb90bf767bef7837ae62d57e
baklanovp <baklanovp@gmail.com>
2018-03-12 18:53:07 +0300

    -ADD: reading Hyd

--- pystella/model/h5stella.py
+++ pystella/model/h5stella.py
@@ -1,3 +1,8 @@
+##
+#  Interface to read stella h5-file
+#  Usage example: >>> python3 h5stella.py  stella-model.h5
+#
+
 import os
 import logging
 
@@ -48,7 +53,21 @@ class H5Stella(object):
             res = H5Fj('Fj').fill(h5f)
         return res
 
+    @property
+    def Hyd(self):
+        logging.debug('Hyd from {}'.format(self.Fname))
+        with h5py.File(self.Fname, "r") as h5f:
+            res = H5Hyd('Hyd').fill(h5f)
+        return res
+
     def ds_write(self, path, ds, attrs=None):
+        """
+        Write to h5-file dataset.
+        :param path: The path inside h5-file
+        :param ds: data to write
+        :param attrs: Additional information. Default: None
+        :return: void
+        """
         if path == '':
             raise ValueError('path')
         if ds is None:
@@ -68,6 +87,7 @@ class H5TimeElement(object):
     def __init__(self, name, path):
         self._name = name
         self._path = path
+        self._attrs = None  # Attributes
         self._s = None  # step
         self._t = None  # time
         self._v = None  # value
@@ -81,19 +101,33 @@ class H5TimeElement(object):
         return self._name
 
     @property
+    def Attrs(self):
+        return self._attrs
+
+    @property
     def S(self):
+        """
+        Step
+        :return:
+        """
         return self._s
 
     @property
     def T(self):
+        """
+        Time
+        :return:
+        """
         return self._t
 
     @property
     def V(self):
+        """Value"""
         return self._v
 
     @property
     def Shape(self):
+        """The shape of data"""
         return self._v.shape
 
     @property
@@ -101,17 +135,18 @@ class H5TimeElement(object):
         return self.Shape[0]
 
     @property
-    def Nfreq(self):
-        return self.Shape[1]
-
-    @property
     def Nzon(self):
-        return self.Shape[2]
+        return self.Shape[1]
 
     def fill(self, h5f):
         self._s = np.array(h5f.get(self.Path + '/step'))  # step
         self._t = np.array(h5f.get(self.Path + '/time'))  # time
         self._v = np.array(h5f.get(self.Path + '/value'))  # value
+        attrs = h5f.get(self.Path).attrs
+        if attrs is not None:
+            self._attrs = {}
+            for k, v in attrs.items():
+                self._attrs[k] = v
         return self
 
     def IdxByTime(self, time):
@@ -133,16 +168,79 @@ class H5TimeElement(object):
         return self.V[:, :, nz]
 
     def Info(self):
+        print("Ntime={} self.Nzon={}".format(self.Ntime, self.Nzon))
+
+
+class H5FreqTimeElement(H5TimeElement):
+    def __init__(self, name, path):
+        super(H5FreqTimeElement, self).__init__(name, path)
+
+    @property
+    def Nfreq(self):
+        return self.Shape[1]
+
+    @property
+    def Nzon(self):
+        return self.Shape[2]
+
+    def Info(self):
         print("Ntime={} self.Nzon={} Nfreq={} ".format(self.Ntime, self.Nzon, self.Nfreq))
 
 
-class H5Fh(H5TimeElement):
+class H5Fh(H5FreqTimeElement):
     def __init__(self, name):
         self._name = name
         super(H5Fh, self).__init__(name, path='/timing/Fh')
 
 
-class H5Fj(H5TimeElement):
+class H5Fj(H5FreqTimeElement):
     def __init__(self, name):
         self._name = name
         super(H5Fj, self).__init__(name, path='/timing/Fj')
+
+
+class H5Hyd(H5TimeElement):
+    def __init__(self, name):
+        self._name = name
+        super(H5Hyd, self).__init__(name, path='/timing/Hyd')
+
+    @property
+    def Nvars(self):
+        return self.Shape[-1]
+
+
+def main():
+    import matplotlib.pyplot as plt
+
+    if len(sys.argv) < 2:
+        print('Missed  h5-file')
+        exit()
+
+    fname = sys.argv[1]  # 'dumm030307mh5.h5'
+    h5 = H5Stella(fname)
+
+    # read frequencies vars
+    Fj = h5.Fj
+    Fj.Info()
+
+    # plot Hyd
+    hyd = h5.Hyd
+    hyd.Info()
+
+    nums = [0, 2, 5, 10, 20, 50]  # time steps
+    columns = hyd.Attrs['columns'].split()
+    plt.figure(figsize=(12, 12))
+    for i, var in enumerate(columns):
+        for num in nums:
+            plt.subplot(2, 2, i + 1)
+            x = hyd.V[num, :, 0]
+            y = hyd.V[num, :, i]
+            plt.plot(x, y, '-', label='{} t={:.3f}'.format(str(var, 'utf-8'), hyd.T[num]))
+            plt.xscale('log')
+            plt.yscale('log')
+        plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
